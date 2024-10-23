1870c53d35472a9c4214272576deb9c8ae089bf9
Mihai Doinea <mihai.doinea@oracle.com>
2024-10-23 12:51:09 +0300

    G1096_S04

--- G1096_S04/app/src/main/java/ro/ase/ie/g1096_s04/MainActivity.java
+++ G1096_S04/app/src/main/java/ro/ase/ie/g1096_s04/MainActivity.java
@@ -1,6 +1,8 @@
 package ro.ase.ie.g1096_s04;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewStub;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewStub viewStub;
+    View inflatedView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        initializeControls();
     }
+
+    private void initializeControls() {
+        viewStub = findViewById(R.id.viewStub);
+        inflatedView = viewStub.inflate();
+    }
+
+    public void showLayout(View view)
+    {
+        viewStub.setVisibility(View.VISIBLE);
+    }
+
+    public void hideLayout(View view)
+    {
+        viewStub.setVisibility(View.GONE);
+    }
+
 }
