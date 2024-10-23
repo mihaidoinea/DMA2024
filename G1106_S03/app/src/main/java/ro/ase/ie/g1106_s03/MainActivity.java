b8cdd642b07b2705eb60f9c978f84338e5d4936f
Mihai Doinea <mihai.doinea@oracle.com>
2024-10-23 10:23:58 +0300

    G1106_S04

--- G1106_S03/app/src/main/java/ro/ase/ie/g1106_s03/MainActivity.java
+++ G1106_S03/app/src/main/java/ro/ase/ie/g1106_s03/MainActivity.java
@@ -1,14 +1,25 @@
 package ro.ase.ie.g1106_s03;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewStub;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity extends AppCompatActivity {
+
+    ViewStub viewStub;
+    View inflatedView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,48 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        viewStub = findViewById(R.id.viewStub);
+        inflatedView = viewStub.inflate();
+        Button button = inflatedView.findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(MainActivity.this, inflatedView, "Hello from SnackBar!", Snackbar.LENGTH_LONG)
+                        .setAction("Redo action", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(MainActivity.this,"Hello from SecondActivity button!", Toast.LENGTH_LONG).show();
+                            }
+                        }).show();
+
+            }
+        });
+    }
+
+    public void showView(View view)
+    {
+        viewStub.setVisibility(View.VISIBLE);
+    }
+    public void hideView(View view)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setCancelable(true)
+                .setTitle("Please confirm action...")
+                .setMessage("Do you really want to hide the view?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        viewStub.setVisibility(View.GONE);
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this,"Hello from SecondActivity button!", Toast.LENGTH_LONG).show();
+                    }
+                });
+        builder.show();
+
     }
 }
