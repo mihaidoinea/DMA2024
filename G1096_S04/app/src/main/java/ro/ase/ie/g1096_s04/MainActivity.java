b924e769448715c02c607648f665040109170f77
Mihai Doinea <mihai.doinea@oracle.com>
2024-10-23 13:03:57 +0300

    G1096_S04

--- G1096_S04/app/src/main/java/ro/ase/ie/g1096_s04/MainActivity.java
+++ G1096_S04/app/src/main/java/ro/ase/ie/g1096_s04/MainActivity.java
@@ -1,8 +1,14 @@
 package ro.ase.ie.g1096_s04;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.ViewStub;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -32,6 +38,25 @@ public class MainActivity extends AppCompatActivity {
     private void initializeControls() {
         viewStub = findViewById(R.id.viewStub);
         inflatedView = viewStub.inflate();
+        Button button = inflatedView.findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addControl();
+            }
+        });
+    }
+    int counter =0;
+
+    private void addControl() {
+        Button button = new Button(MainActivity.this);
+        button.setText("Button_" + (++counter));
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        int hPixels = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50, displayMetrics);
+        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, hPixels);
+        button.setLayoutParams(layoutParams);
+        LinearLayout linearLayout = inflatedView.findViewById(R.id.extraLayout);
+        linearLayout.addView(button);
     }
 
     public void showLayout(View view)
