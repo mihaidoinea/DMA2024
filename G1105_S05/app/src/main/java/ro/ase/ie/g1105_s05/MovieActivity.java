dbf52db4ad56a749aad23add44df544d43ff0197
Mihai Doinea <mihai.doinea@oracle.com>
2024-11-05 09:59:45 +0200

    G1105_S06

--- G1105_S05/app/src/main/java/ro/ase/ie/g1105_s05/MovieActivity.java
+++ G1105_S05/app/src/main/java/ro/ase/ie/g1105_s05/MovieActivity.java
@@ -1,6 +1,20 @@
 package ro.ase.ie.g1105_s05;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.RatingBar;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +22,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MovieActivity extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
+public class MovieActivity extends AppCompatActivity  {
+
+    private EditText etTitle;
+    private EditText etBudget;
+    private EditText etRelease;
+    private EditText etPoster;
+    private Spinner spGenre;
+    private SeekBar sbDuration;
+    private RatingBar rbRating;
+    private Switch swRecommended;
+    private RadioGroup rgApproval;
+    private Button btnMovieAction;
+    private String MovieTag = MovieActivity.class.getSimpleName();
+    private Calendar calendar = Calendar.getInstance();
+    private SimpleDateFormat sdf;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +50,101 @@ public class MovieActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        sdf = new SimpleDateFormat("dd-MM-YYYY");
+
+        initializeControls();
+
+        initializeEvents();
+    }
+
+    private void initializeEvents() {
+        etRelease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DatePickerDialog.OnDateSetListener onDateSetListener = new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+                    }
+                };
+                DatePickerDialog dpd = new DatePickerDialog(MovieActivity.this,
+                        onDateSetListener,
+                        calendar.get(Calendar.YEAR),
+                        calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH));
+                dpd.show();
+            }
+        });
+
+        spGenre.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        sbDuration.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        rbRating.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+
+            }
+        });
+
+        swRecommended.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            }
+        });
+
+        rgApproval.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+            }
+        });
+
+        btnMovieAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(MovieTag, "Action triggered for the movie.");
+            }
+        });
+    }
+
+    private void initializeControls() {
+        etTitle = findViewById(R.id.etTitle);
+        etBudget = findViewById(R.id.etBudget);
+        etRelease = findViewById(R.id.etRelease);
+        etPoster = findViewById(R.id.etPoster);
+        swRecommended = findViewById(R.id.swRecommended);
+        rbRating = findViewById(R.id.rbRating);
+        rgApproval = findViewById(R.id.rgApproval);
+        sbDuration = findViewById(R.id.sbDuration);
+        spGenre = findViewById(R.id.spGenre);
+        btnMovieAction = findViewById(R.id.btnMovieAction);
     }
 }
