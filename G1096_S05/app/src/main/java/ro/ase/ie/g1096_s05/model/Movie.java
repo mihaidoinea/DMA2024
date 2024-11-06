1b779e4986555e6a069935c8f35690e31d7a759c
Mihai Doinea <mihai.doinea@oracle.com>
2024-11-06 12:13:55 +0200

    G1096_S05

--- G1096_S05/app/src/main/java/ro/ase/ie/g1096_s05/model/Movie.java
+++ G1096_S05/app/src/main/java/ro/ase/ie/g1096_s05/model/Movie.java
@@ -10,15 +10,20 @@ public class Movie {
     private Boolean recommended;
     private GenreEnum genre;
     private ParentalApprovalEnum parentalApproval;
+    private String posterUrl;
+    private Float rating;
+    private Double budget;
 
-    public Movie(String title, Date release, Integer duration,
-                 Boolean recommended, GenreEnum genre, ParentalApprovalEnum status) {
+    public Movie(String title, Date release, Integer duration, Boolean recommended, GenreEnum genre, ParentalApprovalEnum parentalApproval, String posterUrl, Float rating, Double budget) {
         this.title = title;
         this.release = release;
         this.duration = duration;
         this.recommended = recommended;
         this.genre = genre;
-        this.parentalApproval = status;
+        this.parentalApproval = parentalApproval;
+        this.posterUrl = posterUrl;
+        this.rating = rating;
+        this.budget = budget;
     }
 
     @Override
@@ -30,6 +35,9 @@ public class Movie {
                 ", recommended=" + recommended +
                 ", genre=" + genre +
                 ", parentalApproval=" + parentalApproval +
+                ", posterUrl='" + posterUrl + '\'' +
+                ", rating=" + rating +
+                ", budget=" + budget +
                 '}';
     }
 }
